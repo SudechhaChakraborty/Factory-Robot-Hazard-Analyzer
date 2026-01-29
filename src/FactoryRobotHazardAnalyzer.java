cebb8f9711af8d4823f1ac053f8973fe7e6ed9ab
Sudechha <sudechhachakraborty@gmail.com>
2026-01-29 15:01:27 +0530

    [Sudechha] : [Added] : UC7: Refactor machinery state risk mapping into helper method

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -49,21 +49,26 @@ public class FactoryRobotHazardAnalyzer {
             );
         }
 
-        double machineRiskFactor;
-
-        if (machineryState.equals("Worn")) {
-            machineRiskFactor = 1.3;
-        } else if (machineryState.equals("Faulty")) {
-            machineRiskFactor = 2.0;
-        } else if (machineryState.equals("Critical")) {
-            machineRiskFactor = 3.0;
-        } else {
-            throw new RobotSafetyException(
-                    "Error: Unsupported machinery state"
-            );
-        }
+        double machineRiskFactor = getMachineRiskFactor(machineryState);
 
         return ((1.0 - armPrecision) * 15.0)
                 + (workerDensity * machineRiskFactor);
+    }
+
+    public static double getMachineRiskFactor(String machineryState)
+            throws RobotSafetyException {
+
+        switch (machineryState) {
+            case "Worn":
+                return 1.3;
+            case "Faulty":
+                return 2.0;
+            case "Critical":
+                return 3.0;
+            default:
+                throw new RobotSafetyException(
+                        "Error: Unsupported machinery state"
+                );
+        }
     }
 }
